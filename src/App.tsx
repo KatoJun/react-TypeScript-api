41c0e3e679a6b00640e46880bfa4c0f63052b7c5
KatoJun <jun.kato0208@gmail.com>
2021-10-27 11:59:56 +0900

    型のせいでバグっている例

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
+import { Todo } from "./Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
+
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((result) => {
-      console.log(result);
+      setTodos(result.data);
     });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} userId={todo.userid} />
+      ))}
     </div>
   );
 }
