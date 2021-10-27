5ddbf6103cb0a21928449e069fb5a37b00318588
KatoJun <jun.kato0208@gmail.com>
2021-10-27 12:31:03 +0900

    PickとOmit 既存の型から一部を抽出したり省いたりして新しい型を定義

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,10 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
+import { TodoType } from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+// Pickを使うと既存の型から必要なプロパティを部分的に抜き出せる
+// Pick<TodoType, "userId" | "title" | "completed"
+
+// OmitはPickの逆で既存の型から省くプロパティを指定する
+export const Todo = (props: Omit<TodoType, "id">) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー：${userId})`}</p>;
