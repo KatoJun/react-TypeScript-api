187c7228ada5dd4a5703fc4c639adbddd7b8386e
KatoJun <jun.kato0208@gmail.com>
2021-10-27 12:45:20 +0900

    関数コンポーネント自体の型定義 FC, VFC

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,11 @@
+import { VFC } from "react";
 import { TodoType } from "./types/todo";
 
 // Pickを使うと既存の型から必要なプロパティを部分的に抜き出せる
 // Pick<TodoType, "userId" | "title" | "completed"
 
 // OmitはPickの逆で既存の型から省くプロパティを指定する
-export const Todo = (props: Omit<TodoType, "id">) => {
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー：${userId})`}</p>;
